7ae1698c2c03b12acfcfcd2cb377b01e02bbc941
md-hasan-ali <hasanaliringku@gmail.com>
2021-08-21 18:20:23 +0600

    final commit

--- js/imac.js
+++ js/imac.js
@@ -36,38 +36,38 @@ function updatePrice() {
 }
 
 // addEvenListener added for memory button
-memory1.addEventListener('click', function () {
+memory1.addEventListener('click', function() {
     memoryCost.innerText = '0';
     updatePrice();
 });
-memory2.addEventListener('click', function () {
+memory2.addEventListener('click', function() {
     memoryCost.innerText = '180';
     updatePrice();
 })
 // addEvenListener added for storage button
-storage1.addEventListener('click', function () {
+storage1.addEventListener('click', function() {
     storageCost.innerText = '0';
     updatePrice();
 })
-storage2.addEventListener('click', function () {
+storage2.addEventListener('click', function() {
     storageCost.innerText = '100';
     updatePrice();
 })
-storage3.addEventListener('click', function () {
+storage3.addEventListener('click', function() {
     storageCost.innerText = '180';
     updatePrice();
 })
 // addEvenListener added for delivery button
-freeDelivery.addEventListener('click', function () {
+freeDelivery.addEventListener('click', function() {
     deliveryCost.innerText = '0';
     updatePrice();
 })
-chargeDelivery.addEventListener('click', function () {
+chargeDelivery.addEventListener('click', function() {
     deliveryCost.innerText = '20';
     updatePrice();
 })
 // get promo button and call the promo code function 
-cuponButton.addEventListener('click', function () {
+cuponButton.addEventListener('click', function() {
     // get cupon field 
     const cuponField = document.getElementById('cupon-field');
     const allTotal = mainTotal.innerText;
